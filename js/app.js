773e5bc7cf4c293fd52e6cbf2931376c2e06b794
Nesto <bumbolio@gmail.com>
2017-05-29 23:42:17 -0400

    Challenge 4 solution

--- js/app.js
+++ js/app.js
@@ -231,3 +231,25 @@ menu.Food.forEach(function(category) {
 
     menuElement.innerHTML += table;
 });
+
+
+$('#email-field').keyup(function(){
+  if(this.value !== '' && !isValidEmailAddress(this.value)) {
+    $('#email-validation-message').show();
+    $('#email-submit').prop('disabled', true);
+  } else {
+    $('#email-validation-message').hide();
+    $('#email-submit').prop('disabled', false);
+  }
+});
+
+$('#email-submit').click(function(e){
+  e.preventDefault();
+  alert('Server unavaliable, try again later!');
+});
+
+//Checks if email address is valid.  
+function isValidEmailAddress(emailAddress) {
+    var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+    return pattern.test(emailAddress);
+};
